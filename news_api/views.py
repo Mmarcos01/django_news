a161652421000f1d0c96773c0f17997415459e93
Mmarcos01 <marie.marcos25@gmail.com>
2022-01-09 13:59:19 -0800

    edit views and form options

--- news_api/views.py
+++ news_api/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 import requests
 
-# move to env
-API_KEY = '141063e5e50941c9a61678a0beacfc78'
+
 
 
 def Home(request):
-    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
-    response = requests.get(url)
-    data = response.json() 
-    articles = data['articles']
-    
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json() 
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json() 
+        articles = data['articles']
+
     context = {
         'articles' : articles
     }
