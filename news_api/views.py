52cdf2ad0300bd71f2c0599738454dc685f8cb54
Mmarcos01 <marie.marcos25@gmail.com>
2022-01-09 12:43:18 -0800

    create app urls, views

--- news_api/views.py
+++ news_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+
+
+def Home(request):
+    url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
+    response = requests.get(url)
+    # convert to json
+    data = response.json() 
+    print(data)
