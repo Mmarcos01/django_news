b95c309ed537b7f6f6fd77493c7d8009f19f9edf
Mmarcos01 <marie.marcos25@gmail.com>
2022-01-09 14:57:53 -0800

    edit views and form options, styles

--- news_api/views.py
+++ news_api/views.py
@@ -7,16 +7,16 @@ import requests
 def Home(request):
     country = request.GET.get('country')
     category = request.GET.get('category')
-    if country:
-        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
-        response = requests.get(url)
-        data = response.json() 
-        articles = data['articles']
-    else:
-        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
-        response = requests.get(url)
-        data = response.json() 
-        articles = data['articles']
+    # if country:
+    #     url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+    #     response = requests.get(url)
+    #     data = response.json() 
+    #     articles = data['articles']
+    # else:
+    url = f'https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json() 
+    articles = data['articles']
 
     context = {
         'articles' : articles
